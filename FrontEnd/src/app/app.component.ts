906081e512f711ad7174d8d877b47ef8ada67dc9
nolwenndreyer <nolwenn.dreyer@etu.univ-lyon1.fr>
2024-03-17 17:27:48 -0400

    olok-82: mise en place du routing

--- FrontEnd/src/app/app.component.ts
+++ FrontEnd/src/app/app.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Router, RouterOutlet} from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from "./profile/profile.component";
 
 @Component({
   selector: 'app-root',
+  template: `
+    <router-outlet></router-outlet>
+  `,
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [
+    RouterOutlet
+  ],
+  styles: []
 })
+
 export class AppComponent {
-  title = 'FrontEnd';
+  constructor(private router: Router) {
+    this.router.resetConfig([
+      { path: '', redirectTo: '/login', pathMatch: 'full' },
+      { path: 'login', component: LoginComponent },
+      { path: 'home', component: HomeComponent },
+      { path: 'profile', component: ProfileComponent}
+      // Ajoutez d'autres routes selon vos besoins
+    ]);
+  }
 }
