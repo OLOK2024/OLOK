d1630485a8cbc051c477cfa6dcb8d6301f1cc78a
leJBP <jb.prevost62@gmail.com>
2024-04-21 18:58:53 -0400

    olok-85: allongement lifetime token

--- FrontEnd/src/app/token.interceptor.ts
+++ FrontEnd/src/app/token.interceptor.ts
@@ -9,7 +9,7 @@ export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
   if (token) {
     // clone la requête et ajoute le token dans l'en-tête Authorization
     const newReq = req.clone({
-      headers: req.headers.set('Authorization', `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzEzNzM5Nzg0LCJpYXQiOjE3MTM3Mzk0ODQsImp0aSI6ImI4Zjk1Njg3Yjk0MDRhNmI4MGE0NzgxZGY4ODU1ZTFiIiwidXNlcl9pZCI6MX0.AWiqtuyGQsdgTfMJA0cwx3Zh2T_wTqEpq5BzHZ1k0AQ`),
+      headers: req.headers.set('Authorization', `Bearer ${token}`),
     });
     return next(newReq);
   }
