bd7e0ed7e9cb6abe7a2a659d5613f3a6594ada4c
leJBP <jb.prevost62@gmail.com>
2024-04-22 12:40:31 -0400

    olok-85: fix bug refresh

--- FrontEnd/src/app/token.interceptor.ts
+++ FrontEnd/src/app/token.interceptor.ts
@@ -2,16 +2,20 @@ import { HttpInterceptorFn } from '@angular/common/http';
 
 export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
   // récupère le token JWT du localStorage
-  const token = localStorage.getItem('token');
-  console.log(token);
 
-  // vérifie si le token existe
-  if (token && token != "undefined") {
-    // clone la requête et ajoute le token dans l'en-tête Authorization
-    const newReq = req.clone({
-      headers: req.headers.set('Authorization', `Bearer ${token}`),
-    });
-    return next(newReq);
+  try {
+    const token = localStorage.getItem('token');
+    console.log(token);
+    // vérifie si le token existe
+    if (token && token != "undefined") {
+      // clone la requête et ajoute le token dans l'en-tête Authorization
+      const newReq = req.clone({
+        headers: req.headers.set('Authorization', `Bearer ${token}`),
+      });
+      return next(newReq);
+  }
+  } catch (error) {
+    console.error('Error getting token from localStorage:', error);
   }
 
   // transmet la requête modifiée ou originale
